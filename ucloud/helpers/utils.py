5e4c62d4e7f59cc21e16600a890386c7bb9fb267
yufei.li <yufei.li@ucloud.cn>
2019-06-18 16:48:51 +0800

    update ci

--- ucloud/helpers/utils.py
+++ ucloud/helpers/utils.py
@@ -37,8 +37,9 @@ def gen_password(
     :param int min_specials: minimal number of special letters
     :return:
     """
-    all_letters = (lower_letters + upper_letters + number_letters +
-                   special_letters)
+    all_letters = sum([
+        lower_letters, upper_letters, number_letters, special_letters
+    ])
     minimal_total = min_lower + min_upper + min_number + min_specials
     if n < minimal_total:
         raise ValueError((
@@ -46,12 +47,12 @@ def gen_password(
             "total minimal letters number"
         ))
 
-    minimal_letters = (
-        gen_string(lower_letters, min_lower) +
-        gen_string(upper_letters, min_upper) +
-        gen_string(number_letters, min_number) +
+    minimal_letters = sum([
+        gen_string(lower_letters, min_lower),
+        gen_string(upper_letters, min_upper),
+        gen_string(number_letters, min_number),
         gen_string(special_letters, min_specials)
-    )
+    ])
 
     additional_letters = random.sample(all_letters, n - minimal_total)
     results = list(minimal_letters) + additional_letters
