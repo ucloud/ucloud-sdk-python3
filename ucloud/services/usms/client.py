560a6a6ce8c76ad0a3cde9c3f82d79ac17cfc3aa
UCloud <uweb-bot@ucloud.cn>
2021-04-30 19:05:03 +0800

    auto codegen for USMS

--- ucloud/services/usms/client.py
+++ ucloud/services/usms/client.py
@@ -20,24 +20,26 @@ class USMSClient(Client):
 
         **Request**
 
-        - **ProjectId** (str) - (Config) 项目ID，不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list.html>`_
+        - **ProjectId** (str) - (Config) 项目ID，不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list>`_
         - **CertificateType** (int) - (Required) 签名的资质证明文件类型，需与签名类型保持一致，说明如下：0-三证合一/企业营业执照/组织机构代码证书/社会信用代码证书；1-应用商店后台开发者管理截图；2-备案服务商的备案成功截图(含域名，网站名称，备案号)；3-公众号或小程序的管理界面截图；4-商标注册证书；5-组织机构代码证书、社会信用代码证书；
         - **Description** (str) - (Required) 短信签名申请原因
         - **File** (str) - (Required) 短信签名的资质证明文件，需先进行base64编码格式转换，此处填写转换后的字符串。文件大小不超过4 MB
-        - **SigContent** (str) - (Required) 短信签名名称；长度为2-12个字符, 可包含中文、数字和符号；无需填写【】或[]，系统会自动添加
+        - **SigContent** (str) - (Required)
         - **SigPurpose** (int) - (Required) 签名用途，0-自用，1-他用；
         - **SigType** (int) - (Required) 签名类型，说明如下：0-公司或企业的全称或简称；1-App应用的全称或简称；2-工信部备案网站的全称或简称；3-公众号或小程序的全称或简称；4-商标名的全称或简称；5-政府/机关事业单位/其他单位的全称或简称；
+        - **International** (bool) - 国内/国际短信。true:国际短信，false:国内短信，若不传值则默认该值为false
         - **ProxyFile** (str) - 短信签名授权委托文件，需先进行base64编码格式转换，此处填写转换后的字符串。文件大小不超过4 MB；当您是代理并使用第三方的签名时（也即SigPurpose为1-他用），该项为必填项；
 
         **Response**
 
         - **Message** (str) - 返回状态码描述，如果操作成功，默认返回为空
-        - **SigContent** (str) - 短信签名名称
         - **SigId** (str) - 短信签名ID（短信签名申请时的工单ID）
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id}
+        d = {
+            "ProjectId": self.config.project_id,
+        }
         req and d.update(req)
         d = apis.CreateUSMSSignatureRequestSchema().dumps(d)
 
@@ -54,14 +56,13 @@ class USMSClient(Client):
 
         **Request**
 
-        - **ProjectId** (str) - (Config) 项目ID，不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list.html>`_
-        - **Region** (str) - (Config) 地域。 参见  `地域和可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
+        - **ProjectId** (str) - (Config) 项目ID，不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list>`_
+        - **Region** (str) - (Config) 地域。 参见  `地域和可用区列表 <https://docs.ucloud.cn/api/summary/regionlist>`_
         - **Purpose** (int) - (Required) 短信模板用途类型：1-验证码类短信模板；2-系统通知类短信模板；3-会员推广类短信模板；
         - **Template** (str) - (Required) 短信模板内容，说明如下：字数不超过500，每个中文、符号、英文、数组等都计为一个字；模板中的变量填写格式：{N}，其中N为大于1的整数，有多个参数时，建议N从1开始顺次，例如：{1}、{2}等；短信模板禁止仅包括变量的情况；
         - **TemplateName** (str) - (Required) 短信模板名称，不超过32个字符，每个中文、符号、英文、数字等都计为1个字。
+        - **International** (bool) - 标记是否为国际短信。true:国际短信，false:国内短信，若不传值则默认该值为false
         - **Remark** (str) - 短信模板申请原因说明，字数不超过128，每个中文、符号、英文、数字等都计为1个字。
-        - **UnsubscribeInfo** (str) - 当Purpose为3时，也即会员推广类短信模板，该项必填。枚举值：TD退订、回T退订、回N退订、回TD退订、退订回T、退订回D、退订回TD、退订回复T、退订回复D、退订回复N、退订回复TD、拒收回T
-        - **Zone** (str) - 可用区。参见  `可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
 
         **Response**
 
@@ -70,7 +71,10 @@ class USMSClient(Client):
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id, "Region": self.config.region}
+        d = {
+            "ProjectId": self.config.project_id,
+            "Region": self.config.region,
+        }
         req and d.update(req)
         d = apis.CreateUSMSTemplateRequestSchema().dumps(d)
 
@@ -96,7 +100,9 @@ class USMSClient(Client):
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id}
+        d = {
+            "ProjectId": self.config.project_id,
+        }
         req and d.update(req)
         d = apis.DeleteUSMSSignatureRequestSchema().dumps(d)
 
@@ -113,7 +119,6 @@ class USMSClient(Client):
         - **ProjectId** (str) - (Config) 项目ID。不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list.html>`_
         - **Region** (str) - (Config) 地域。 参见  `地域和可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
         - **TemplateIds** (list) - (Required) 模板ID（也即短信模板申请时的工单ID），支持以数组的方式，举例，以TemplateIds.0、TemplateIds.1...TemplateIds.N方式传入
-        - **Zone** (str) - 可用区。参见  `可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
 
         **Response**
 
@@ -121,7 +126,10 @@ class USMSClient(Client):
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id, "Region": self.config.region}
+        d = {
+            "ProjectId": self.config.project_id,
+            "Region": self.config.region,
+        }
         req and d.update(req)
         d = apis.DeleteUSMSTemplateRequestSchema().dumps(d)
 
@@ -131,7 +139,7 @@ class USMSClient(Client):
     def get_usms_send_receipt(
         self, req: typing.Optional[dict] = None, **kwargs
     ) -> dict:
-        """GetUSMSSendReceipt - 获取短信发送回执信息。下游服务提供商回执信息返回会有一定延时，建议发送完短信以后，5-10分钟后再调用该接口拉取回执信息。若超过12小时未返回，则请联系技术支持确认原因
+        """GetUSMSSendReceipt - 调用接口GetUSMSSendReceipt短信发送状态信息
 
         **Request**
 
@@ -147,22 +155,25 @@ class USMSClient(Client):
 
         **Response Model**
 
-        **ReceiptPerPhone**
+        **ReceiptPerSession**
+        - **ReceiptSet** (list) - 见 **ReceiptPerPhone** 模型定义
+        - **SessionNo** (str) - 发送短信时返回的SessionNo
 
+
+        **ReceiptPerPhone**
         - **CostCount** (int) - 消耗短信条数
         - **Phone** (str) - 手机号码
         - **ReceiptDesc** (str) - 回执结果描述
         - **ReceiptResult** (str) - 回执结果
         - **ReceiptTime** (int) - 回执返回时间
 
-        **ReceiptPerSession**
-
-        - **ReceiptSet** (list) - 见 **ReceiptPerPhone** 模型定义
-        - **SessionNo** (str) - 发送短信时返回的SessionNo
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id, "Region": self.config.region}
+        d = {
+            "ProjectId": self.config.project_id,
+            "Region": self.config.region,
+        }
         req and d.update(req)
         d = apis.GetUSMSSendReceiptRequestSchema().dumps(d)
 
@@ -188,15 +199,17 @@ class USMSClient(Client):
         **Response Model**
 
         **OutSignature**
-
         - **ErrDesc** (str) - 签名审核失败原因
         - **SigContent** (str) - 签名内容
         - **SigId** (str) - 签名ID
         - **Status** (int) - 签名状态。0-待审核 1-审核中 2-审核通过 3-审核未通过 4-被禁用
 
+
         """
         # build request
-        d = {"ProjectId": self.config.project_id}
+        d = {
+            "ProjectId": self.config.project_id,
+        }
         req and d.update(req)
         d = apis.QueryUSMSSignatureRequestSchema().dumps(d)
 
@@ -221,7 +234,6 @@ class USMSClient(Client):
         **Response Model**
 
         **OutTemplate**
-
         - **CreateTime** (int) - 创建时间
         - **ErrDesc** (str) - 审核失败原因
         - **Purpose** (int) - 模板类型，选项：1-验证码类 2-通知类 3-会员推广类
@@ -232,9 +244,12 @@ class USMSClient(Client):
         - **TemplateName** (str) - 短信模板名称
         - **UnsubscribeInfo** (str) - 退订信息；一般填写方式“回T退订”，当purpose为3（也即会员推广类）时，为必填项
 
+
         """
         # build request
-        d = {"ProjectId": self.config.project_id}
+        d = {
+            "ProjectId": self.config.project_id,
+        }
         req and d.update(req)
         d = apis.QueryUSMSTemplateRequestSchema().dumps(d)
 
@@ -244,28 +259,31 @@ class USMSClient(Client):
     def send_usms_message(
         self, req: typing.Optional[dict] = None, **kwargs
     ) -> dict:
-        """SendUSMSMessage - 发送短信息。短信字数超过70个后，按照每66个进行切割(因为要加上1/3), 2/3)等字样，占用4个字长)。短信最大长度不能超过600个字。每个汉字、数字、字母、字符都按一个字计
+        """SendUSMSMessage - 调用接口SendUSMSMessage发送短信
 
         **Request**
 
-        - **ProjectId** (str) - (Config) 项目ID。不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list.html>`_
-        - **Region** (str) - (Config) 地域。 参见  `地域和可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
+        - **ProjectId** (str) - (Config) 项目ID。不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list>`_
+        - **Region** (str) - (Config) 地域。 参见  `地域和可用区列表 <https://docs.ucloud.cn/api/summary/regionlist>`_
         - **PhoneNumbers** (list) - (Required) 电话号码数组，电话号码格式为(60)1xxxxxxxx，()中为国际长途区号(如中国为86或0086，两种格式都支持)，后面为电话号码.若不传入国际区号，如1851623xxxx，则默认为国内手机号
-        - **TemplateId** (str) - (Required) 模板ID。若指定的模板ID审核未通过(status不等于2)则不允许发送
-        - **TemplateParams** (list) - (Required) 模板参数数组，以TempalteParams.0，TempalteParams.1.。。格式。若模板ID指定的模板无可变参数，则不传入该参数。模板参数个数与模板不匹配，则不允许发送
-        - **SigContent** (str) - 使用的签名，如果不输入则使用默认签名，若没有申请默认签名不允许发送；若输入的签名没有申请，则无法发送
-        - **Zone** (str) - 可用区。参见  `可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
+        - **SigContent** (str) - (Required) 短信签名内容，请到 `USMS控制台 <https://console.ucloud.cn/usms>`_ 的签名管理页面查看；使用的短信签名必须是已申请并且通过审核；
+        - **TemplateId** (str) - (Required) 模板ID（也即短信模板申请时的工单ID），请到 `USMS控制台 <https://console.ucloud.cn/usms>`_ 的模板管理页面查看；使用的短信模板必须是已申请并通过审核；
+        - **ExtendCode** (str) - 短信扩展码，格式为阿拉伯数字串，默认不开通，如需开通请联系 UCloud技术支持
+        - **TemplateParams** (list) - 模板可变参数，以数组的方式填写，举例，TempalteParams.0，TempalteParams.1，... 若模板中无可变参数，则该项可不填写；若模板中有可变参数，则该项为必填项，参数个数需与变量个数保持一致，否则无法发送；
+        - **UserId** (str) - 自定义的业务标识ID，字符串（ 长度不能超过32 位）
 
         **Response**
 
-        - **Action** (str) - 操作名称
         - **Message** (str) - 发生错误时表示错误描述
-        - **RetCode** (int) - 返回码
         - **SessionNo** (str) - 本次提交发送的短信的唯一ID，可根据该值查询本次发送的短信列表
+        - **UserId** (str) - 本次提交的自定义业务标识ID，仅当发送时传入有效的UserId，才返回该字段。
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id, "Region": self.config.region}
+        d = {
+            "ProjectId": self.config.project_id,
+            "Region": self.config.region,
+        }
         req and d.update(req)
         d = apis.SendUSMSMessageRequestSchema().dumps(d)
 
@@ -282,14 +300,16 @@ class USMSClient(Client):
 
         **Request**
 
-        - **ProjectId** (str) - (Config) 项目ID，不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list.html>`_
-        - **File** (str) - (Required) 短信签名的资质证明文件，需先进行base64编码格式转换，此处填写转换后的字符串。文件大小不超过4 MB
-        - **SigContent** (str) - (Required) 新的短信签名名称；长度为2-12个字符, 可包含中文、数字和符号；无需填写【】或[]，系统会自动添加
+        - **ProjectId** (str) - (Config) 项目ID，不填写为默认项目，子帐号必须填写。 请参考 `GetProjectList接口 <https://docs.ucloud.cn/api/summary/get_project_list>`_
+        - **SigContent** (str) - (Required) 新的短信签名内容；长度为2-12个字符, 可包含中文、数字和符号；无需填写【】或[]，系统会自动添加
         - **SigId** (str) - (Required) 签名ID（也即短信签名申请时的工单ID），支持以数组的方式，举例，以SigIds.0、SigIds.1...SigIds.N方式传入
         - **SigPurpose** (int) - (Required) 签名用途，0-自用，1-他用；
         - **SigType** (int) - (Required) 签名类型，说明如下：0-公司或企业的全称或简称；1-App应用的全称或简称；2-工信部备案网站的全称或简称；3-公众号或小程序的全称或简称；4-商标名的全称或简称；5-政府/机关事业单位/其他单位的全称或简称；
         - **CertificateType** (int) - 签名的资质证明文件类型，需与签名类型保持一致，说明如下：0-三证合一/企业营业执照/组织机构代码证书/社会信用代码证书；1-应用商店后台开发者管理截图；2-备案服务商的备案成功截图(含域名，网站名称，备案号)；3-公众号或小程序的管理界面截图；4-商标注册证书；5-组织机构代码证书、社会信用代码证书；
-        - **ProxyFile** (str) - 短信签名授权委托文件，需先进行base64编码格式转换，此处填写转换后的字符串。文件大小不超过4 MB；当您是代理并使用第三方的签名时（也即SigPurpose为1-他用），该项为必填项；
+        - **Document** (str) - 短信签名的资质证明文件URL，若未更改审核材料，则该处使用已上传审核材料的URL链接，否则使用File参数
+        - **File** (str) - 短信签名的资质证明文件内容，需先进行base64编码格式转换，此处填写转换后的字符串。文件大小不超过4 MB。内容格式如下: [file type];[code type],[base64]  如：image/jpeg;base64,5YaF5a65
+        - **ProxyDoc** (str) - 短信签名授权委托文件URL，若未更改授权委托文件，则该处填写已上传的授权委托文件的URL链接，否则使用ProxyFile参数
+        - **ProxyFile** (str) - 短信签名授权委托文件内容，需先进行base64编码格式转换，此处填写转换后的字符串。文件大小不超过4 MB；当您是代理并使用第三方的签名时（也即SigPurpose为1-他用），该项为必填项；格式和File类似。
 
         **Response**
 
@@ -297,7 +317,9 @@ class USMSClient(Client):
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id}
+        d = {
+            "ProjectId": self.config.project_id,
+        }
         req and d.update(req)
         d = apis.UpdateUSMSSignatureRequestSchema().dumps(d)
 
@@ -317,8 +339,6 @@ class USMSClient(Client):
         - **TemplateId** (str) - (Required) 短信模板ID
         - **Remark** (str) - 短信模板申请原因说明，字数不超过128，每个中文、符号、英文、数字等都计为1个字。
         - **TemplateName** (str) - 新的模板名称。小于等于32个字，每个中文、英文、数组、符合都计为一个字
-        - **UnsubscribeInfo** (str) - 当Purpose为3时，也即会员推广类短信模板，该项必填。枚举值：TD退订、回T退订、回N退订、回TD退订、退订回T、退订回D、退订回TD、退订回复T、退订回复D、退订回复N、退订回复TD、拒收回T
-        - **Zone** (str) - 可用区。参见  `可用区列表 <https://docs.ucloud.cn/api/summary/regionlist.html>`_
 
         **Response**
 
@@ -326,7 +346,10 @@ class USMSClient(Client):
 
         """
         # build request
-        d = {"ProjectId": self.config.project_id, "Region": self.config.region}
+        d = {
+            "ProjectId": self.config.project_id,
+            "Region": self.config.region,
+        }
         req and d.update(req)
         d = apis.UpdateUSMSTemplateRequestSchema().dumps(d)
 
