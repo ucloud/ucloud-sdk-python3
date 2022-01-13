ea844f2834cc0f3bbb8429c92c8c005a93c42488
UCloud <uweb-bot@ucloud.cn>
2022-01-13 13:45:50 +0800

    Revert "auto codegen for UDTS" (#197)
    This reverts commit f75c3404c26ae7e70c546e926942290014883f02.
    
    Co-authored-by: Yufei Li <yufeiminds@gmail.com>

--- ucloud/services/udts/schemas/apis.py
+++ ucloud/services/udts/schemas/apis.py
@@ -15,12 +15,37 @@ API: CheckUDTSTask
 """
 
 
+class CheckUDTSTaskParamSourceMySQLNodeSyncDataSchema(schema.RequestSchema):
+    """CheckUDTSTaskParamSourceMySQLNodeSyncData -"""
+
+    fields = {
+        "BinlogGTID": fields.Str(required=False, dump_to="BinlogGTID"),
+        "BinlogName": fields.Str(required=False, dump_to="BinlogName"),
+        "BinlogPos": fields.Int(required=False, dump_to="BinlogPos"),
+        "ServerID": fields.Int(required=False, dump_to="ServerID"),
+    }
+
+
 class CheckUDTSTaskParamSourceMySQLNodeQueryDataSchema(schema.RequestSchema):
     """CheckUDTSTaskParamSourceMySQLNodeQueryData -"""
 
     fields = {
         "DBName": fields.Str(required=False, dump_to="DBName"),
         "NewDBName": fields.Str(required=False, dump_to="NewDBName"),
+    }
+
+
+class CheckUDTSTaskParamTargetMySQLNodeSchema(schema.RequestSchema):
+    """CheckUDTSTaskParamTargetMySQLNode -"""
+
+    fields = {
+        "DataRegion": fields.Str(required=False, dump_to="DataRegion"),
+        "Host": fields.Str(required=False, dump_to="Host"),
+        "Password": fields.Str(required=False, dump_to="Password"),
+        "Port": fields.Int(required=False, dump_to="Port"),
+        "SubnetId": fields.Str(required=False, dump_to="SubnetId"),
+        "User": fields.Str(required=False, dump_to="User"),
+        "VPCId": fields.Str(required=False, dump_to="VPCId"),
     }
 
 
@@ -46,14 +71,15 @@ class CheckUDTSTaskParamSourceMySQLNodeSchema(schema.RequestSchema):
     }
 
 
-class CheckUDTSTaskParamSourceMySQLNodeSyncDataSchema(schema.RequestSchema):
-    """CheckUDTSTaskParamSourceMySQLNodeSyncData -"""
+class CheckUDTSTaskParamTargetSchema(schema.RequestSchema):
+    """CheckUDTSTaskParamTarget -"""
 
     fields = {
-        "BinlogGTID": fields.Str(required=False, dump_to="BinlogGTID"),
-        "BinlogName": fields.Str(required=False, dump_to="BinlogName"),
-        "BinlogPos": fields.Int(required=False, dump_to="BinlogPos"),
-        "ServerID": fields.Int(required=False, dump_to="ServerID"),
+        "DataType": fields.Str(required=False, dump_to="DataType"),
+        "MySQLNode": CheckUDTSTaskParamTargetMySQLNodeSchema(
+            required=False, dump_to="MySQLNode"
+        ),
+        "NWType": fields.Str(required=False, dump_to="NWType"),
     }
 
 
@@ -64,33 +90,6 @@ class CheckUDTSTaskParamSourceSchema(schema.RequestSchema):
         "DataType": fields.Str(required=False, dump_to="DataType"),
         "Mode": fields.Str(required=False, dump_to="Mode"),
         "MySQLNode": CheckUDTSTaskParamSourceMySQLNodeSchema(
-            required=False, dump_to="MySQLNode"
-        ),
-        "NWType": fields.Str(required=False, dump_to="NWType"),
-        "ServiceType": fields.Str(required=True, dump_to="ServiceType"),
-    }
-
-
-class CheckUDTSTaskParamTargetMySQLNodeSchema(schema.RequestSchema):
-    """CheckUDTSTaskParamTargetMySQLNode -"""
-
-    fields = {
-        "DataRegion": fields.Str(required=False, dump_to="DataRegion"),
-        "Host": fields.Str(required=False, dump_to="Host"),
-        "Password": fields.Str(required=False, dump_to="Password"),
-        "Port": fields.Int(required=False, dump_to="Port"),
-        "SubnetId": fields.Str(required=False, dump_to="SubnetId"),
-        "User": fields.Str(required=False, dump_to="User"),
-        "VPCId": fields.Str(required=False, dump_to="VPCId"),
-    }
-
-
-class CheckUDTSTaskParamTargetSchema(schema.RequestSchema):
-    """CheckUDTSTaskParamTarget -"""
-
-    fields = {
-        "DataType": fields.Str(required=False, dump_to="DataType"),
-        "MySQLNode": CheckUDTSTaskParamTargetMySQLNodeSchema(
             required=False, dump_to="MySQLNode"
         ),
         "NWType": fields.Str(required=False, dump_to="NWType"),
@@ -131,21 +130,6 @@ API: CreateUDTSTask
 """
 
 
-class CreateUDTSTaskParamSourceMySQLNodeQueryDataSchema(schema.RequestSchema):
-    """CreateUDTSTaskParamSourceMySQLNodeQueryData -"""
-
-    fields = {
-        "DBName": fields.Str(required=False, dump_to="DBName"),
-        "NewDBName": fields.Str(required=False, dump_to="NewDBName"),
-        "TableData": CreateUDTSTaskParamSourceMySQLNodeQueryDataTableDataSchema(
-            required=False, dump_to="TableData"
-        ),
-        "TableMaps": fields.List(
-            CreateUDTSTaskParamSourceMySQLNodeQueryDataTableMapsSchema()
-        ),
-    }
-
-
 class CreateUDTSTaskParamSourceMySQLNodeQueryDataTableDataSchema(
     schema.RequestSchema
 ):
@@ -168,6 +152,32 @@ class CreateUDTSTaskParamSourceMySQLNodeQueryDataTableMapsSchema(
     }
 
 
+class CreateUDTSTaskParamSourceMySQLNodeSyncDataSchema(schema.RequestSchema):
+    """CreateUDTSTaskParamSourceMySQLNodeSyncData -"""
+
+    fields = {
+        "BinlogGTID": fields.Str(required=False, dump_to="BinlogGTID"),
+        "BinlogName": fields.Str(required=False, dump_to="BinlogName"),
+        "BinlogPos": fields.Int(required=False, dump_to="BinlogPos"),
+        "ServerID": fields.Int(required=False, dump_to="ServerID"),
+    }
+
+
+class CreateUDTSTaskParamSourceMySQLNodeQueryDataSchema(schema.RequestSchema):
+    """CreateUDTSTaskParamSourceMySQLNodeQueryData -"""
+
+    fields = {
+        "DBName": fields.Str(required=False, dump_to="DBName"),
+        "NewDBName": fields.Str(required=False, dump_to="NewDBName"),
+        "TableData": CreateUDTSTaskParamSourceMySQLNodeQueryDataTableDataSchema(
+            required=False, dump_to="TableData"
+        ),
+        "TableMaps": fields.List(
+            CreateUDTSTaskParamSourceMySQLNodeQueryDataTableMapsSchema()
+        ),
+    }
+
+
 class CreateUDTSTaskParamSourceMySQLNodeSchema(schema.RequestSchema):
     """CreateUDTSTaskParamSourceMySQLNode -"""
 
@@ -182,9 +192,6 @@ class CreateUDTSTaskParamSourceMySQLNodeSchema(schema.RequestSchema):
         "QueryData": fields.List(
             CreateUDTSTaskParamSourceMySQLNodeQueryDataSchema()
         ),
-        "SSLSecurity": CreateUDTSTaskParamSourceMySQLNodeSSLSecuritySchema(
-            required=False, dump_to="SSLSecurity"
-        ),
         "SubnetId": fields.Str(required=False, dump_to="SubnetId"),
         "SyncData": CreateUDTSTaskParamSourceMySQLNodeSyncDataSchema(
             required=False, dump_to="SyncData"
@@ -192,32 +199,6 @@ class CreateUDTSTaskParamSourceMySQLNodeSchema(schema.RequestSchema):
         "Table": fields.Str(required=False, dump_to="Table"),
         "User": fields.Str(required=False, dump_to="User"),
         "VPCId": fields.Str(required=False, dump_to="VPCId"),
-    }
-
-
-class CreateUDTSTaskParamSourceMySQLNodeSyncDataSchema(schema.RequestSchema):
-    """CreateUDTSTaskParamSourceMySQLNodeSyncData -"""
-
-    fields = {
-        "BinlogGTID": fields.Str(required=False, dump_to="BinlogGTID"),
-        "BinlogName": fields.Str(required=False, dump_to="BinlogName"),
-        "BinlogPos": fields.Int(required=False, dump_to="BinlogPos"),
-        "ServerID": fields.Int(required=False, dump_to="ServerID"),
-    }
-
-
-class CreateUDTSTaskParamSourceSchema(schema.RequestSchema):
-    """CreateUDTSTaskParamSource -"""
-
-    fields = {
-        "BandwidthLimit": fields.Int(required=False, dump_to="BandwidthLimit"),
-        "DataType": fields.Str(required=True, dump_to="DataType"),
-        "Mode": fields.Str(required=True, dump_to="Mode"),
-        "MySQLNode": CreateUDTSTaskParamSourceMySQLNodeSchema(
-            required=False, dump_to="MySQLNode"
-        ),
-        "NWType": fields.Str(required=True, dump_to="NWType"),
-        "ServiceType": fields.Str(required=True, dump_to="ServiceType"),
     }
 
 
@@ -236,6 +217,20 @@ class CreateUDTSTaskParamTargetMySQLNodeSchema(schema.RequestSchema):
     }
 
 
+class CreateUDTSTaskParamSourceSchema(schema.RequestSchema):
+    """CreateUDTSTaskParamSource -"""
+
+    fields = {
+        "BandwidthLimit": fields.Int(required=False, dump_to="BandwidthLimit"),
+        "DataType": fields.Str(required=True, dump_to="DataType"),
+        "Mode": fields.Str(required=True, dump_to="Mode"),
+        "MySQLNode": CreateUDTSTaskParamSourceMySQLNodeSchema(
+            required=False, dump_to="MySQLNode"
+        ),
+        "NWType": fields.Str(required=True, dump_to="NWType"),
+    }
+
+
 class CreateUDTSTaskParamTargetSchema(schema.RequestSchema):
     """CreateUDTSTaskParamTarget -"""
 
@@ -250,27 +245,14 @@ class CreateUDTSTaskParamTargetSchema(schema.RequestSchema):
     }
 
 
-class CreateUDTSTaskParamSourceMySQLNodeSSLSecuritySchema(schema.RequestSchema):
-    """CreateUDTSTaskParamSourceMySQLNodeSSLSecurity -"""
-
-    fields = {
-        "SSLCA": fields.Str(required=False, dump_to="SSLCA"),
-        "SSLCert": fields.Str(required=False, dump_to="SSLCert"),
-        "SSLKey": fields.Str(required=False, dump_to="SSLKey"),
-    }
-
-
 class CreateUDTSTaskRequestSchema(schema.RequestSchema):
     """CreateUDTSTask - 创建UDTS任务"""
 
     fields = {
-        "ChargeType": fields.Str(required=False, dump_to="ChargeType"),
-        "CouponId": fields.Str(required=False, dump_to="CouponId"),
         "IsUnidirection": fields.Str(required=False, dump_to="IsUnidirection"),
         "MaxRetryCount": fields.Str(required=False, dump_to="MaxRetryCount"),
         "Name": fields.Str(required=True, dump_to="Name"),
         "ProjectId": fields.Str(required=False, dump_to="ProjectId"),
-        "Quantity": fields.Int(required=False, dump_to="Quantity"),
         "Query": fields.Str(required=False, dump_to="Query"),
         "Remark": fields.Str(required=False, dump_to="Remark"),
         "Source": fields.List(CreateUDTSTaskParamSourceSchema()),
@@ -287,7 +269,6 @@ class CreateUDTSTaskResponseSchema(schema.ResponseSchema):
     fields = {
         "Data": fields.Str(),
         "Message": fields.Str(required=True, load_from="Message"),
-        "TaskId": fields.Str(required=False, load_from="TaskId"),
     }
 
 
